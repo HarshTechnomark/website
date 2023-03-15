16eab1281b33bb48ad6f517ec5bc0556b822fd51
tmdev <prayag.sonagra@technomark.io>
2023-03-15 12:50:07 +0530

    app file change

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          harsh
+          Prayag
         </a>
       </header>
     </div>
